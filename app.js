8bedd1959ffddbdada67a273b1cc5ea1ab29e8a8
majid shahid <majidshahid474@gmail.com>
2021-04-29 15:57:42 -0700

    js chapter 20 to 40

---
+++ app.js
@@ -0,0 +1,71 @@
+//  var firstNames = ["BlueRay ", "Upchuck ", "Lojack ", "Gizmo ", "Do-Rag "];
+//  var lastNames = ["Zzz", "Burp", "Dogbone", "Droop"];
+//  var fullNames = [];
+//  for (var i = 0; i < firstNames.length; i++) {
+//  for (var j = 0; j < lastNames.length; j++) {
+//  fullNames.push(firstNames[i] + lastNames[j])}}
+//  console.log(fullNames)
+
+
+
+
+// var a = prompt("enter your city name");
+// var b =["karachi","islamabad","peshawar","multan"];
+// var c = a.toLowerCase();
+// for (var i=0;i<=b.length;i++){
+//     if (i==b[i]){
+//        document.write("ok")
+//     }
+//     else{
+//         document.write ("not ")
+//     }
+// }
+
+
+// var  city =prompt("enter your name");
+// var b =city.toLowerCase();
+// console.log(b)
+// var  names =["karachi","islamabad","multan"];
+// for (var i=0;i<names.length;i++){
+//     if (b == names[i]){
+//         document.write("ok")
+       
+
+       
+//     }else{
+//         document.write("nn")
+       
+//     }
+   
+// }
+
+// var a = Math.random()*3;
+// console.log(a)
+// var b =Math.ceil(a)
+// console.log(b)
+// var c = Math.floor(a)
+// console.log(c)
+// var d =a.toFixed(1);
+// console.log(d)
+
+
+
+// var a =Math.random()*6;
+// var b =Math.ceil(a)
+// console.log(b)
+
+
+function tellTime() {
+ var now = new Date();
+ var theHr = now.getHours();
+ console.log(theHr)
+ var theMin = now.getMinutes();
+ console.log(theMin)
+     alert("Current time: "+ theHr + ":" + theMin);
+     }
+     tellTime()
+    
+
+
+
+
